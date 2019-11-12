f9cbfb44e240b69486ae65a516ee0631f062680d
s-wuaar <s-wuaar@53S500035046.it.bsd405.org>
2019-11-12 12:19:37 -0800

    FracCalc Checkpoint 1 - Aaron Wu

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,13 +3,20 @@
  */
 
 package fracCalc;
-
+import java.util.*;
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Welcome to the fraction calculator");
+    	System.out.println("Underscore divides numbers and their fractions for example 1_1/2");
+    	System.out.println("Spaces divide different numbers for example 1 + 2_1/2");
+    	System.out.print("Enter an expression: ");
+    	String input = in.nextLine();
+    	
+    	System.out.println(produceAnswer(input));
+    	in.close();
     }
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -20,11 +27,18 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input) {
         // TODO: Implement this function to produce the solution to the input
-
-        return "";
+    	int space1 = input.indexOf(" ");
+    	String first = input.substring(0, space1);
+    	String operate = input.substring(space1, space1 + 3);
+    	String last = input.substring(space1 + 3);
+    	int num1 = first.charAt(0);
+    	int num2 = last.charAt(0);
+    	//System.out.println(first);
+    	//System.out.println(operate);
+    	//System.out.println(last);
+    	return last;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
