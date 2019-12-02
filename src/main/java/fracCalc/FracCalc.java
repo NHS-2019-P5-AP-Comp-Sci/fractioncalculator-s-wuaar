69727aba7e4b369bb85004fc48b5160b675e400d
s-wuaar <s-wuaar@53S500035046.it.bsd405.org>
2019-12-02 11:45:49 -0800

    Frac Calc Checkpoint 3: Aaron Wu

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,99 +3,164 @@
  */
 
 package fracCalc;
+
 import java.util.*;
+
 public class FracCalc {
-	
-	static int iwhole1;
-	static int iwhole2;
-	static int inumer1;
-	static int inumer2;
-	static int idenom1;
-	static int idenom2;
-	static int recip;
-	
-    public static void main(String[] args) {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-    	Scanner in = new Scanner(System.in);
-    	System.out.println("Welcome to the fraction calculator");
-    	System.out.println("Underscore divides numbers and their fractions for example 1_1/2");
-    	System.out.println("Spaces divide different numbers for example 1 + 2_1/2");
-    	System.out.print("Enter an expression: ");
-    	String input = in.nextLine();
-    	produceAnswer(input);
-    	//System.out.println(produceAnswer(input));
-    	in.close();
-    }
-    //public static int combine() {
-    	
-    //}
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input) {
-        // TODO: Implement this function to produce the solution to the input
-    	int space1 = input.indexOf(" ");
-    	String first = input.substring(0, space1);
-    	String operate = input.substring(space1, space1 + 3);
-    	String last = input.substring(space1 + 3);
-    	String whole1 = "**";
-    	String numer1 = "**";
-    	String denom1 = "**";
-    	if ( first.indexOf("_") > 0 || first.indexOf("/") > 0) {
-    		if ( first.indexOf("_") > 0 ) {
-	    		whole1 = first.substring(0, first.indexOf("_"));
-	    		numer1 = first.substring(first.indexOf("_") + 1, first.indexOf("/"));
-	    		denom1 = first.substring(first.indexOf("/") + 1, first.length());
-    		} else if (first.indexOf("/") > 0) {
-    			whole1 = "0";
-    			numer1 = first.substring(0, first.indexOf("/"));
-    			denom1 = first.substring(first.indexOf("/") + 1, first.length());
-    		}
-    	} else {
-    		whole1 = first;
-    		numer1 = "0";
-    		denom1 = "1";
-    	}
-    	String whole2 = "**";
-    	String numer2 = "**";
-    	String denom2 = "**";
-    	if ( last.indexOf("_") > 0 || last.indexOf("/") > 0) {
-    		if ( last.indexOf("_") > 0) {
-	    		whole2 = last.substring(0, last.indexOf("_"));
-	    		numer2 = last.substring(last.indexOf("_") + 1, last.indexOf("/"));
-	    		denom2 = last.substring(last.indexOf("/") + 1, last.length());
-    		} else if ( last.indexOf("/") > 0) {
-    			whole2 = "0";
-    			numer2 = last.substring(0, last.indexOf("/"));
-    			denom2 = last.substring(last.indexOf("/") + 1, last.length());
-    		}
-    	} else {
-    		whole2 = last;
-    		numer2 = "0";
-    		denom2 = "1";
-    		
-    	}
-    	
-    	//parsing strings to integers after separating the fractions and whole numbers
-    	
-    	iwhole1 = Integer.parseInt(whole1);
-    	iwhole2 = Integer.parseInt(whole2);
-    	inumer1 = Integer.parseInt(numer1);
-    	inumer2= Integer.parseInt(numer2);
-    	idenom1 = Integer.parseInt(denom1);
-    	idenom2 = Integer.parseInt(denom2);
-    	
-    	//combine(iwhole1, iwhole2, inumer1, inumer2, idenom1, idenom2);
-    	
-    	String check2 = "whole:" + iwhole2 + " numerator:" + inumer2 + " denominator:" + idenom2;
+	public static void main(String[] args) {
+		// TODO: Read the input from the user and call produceAnswer with an equation
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to the fraction calculator");
+		System.out.println("Underscore divides numbers and their fractions for example 1_1/2");
+		System.out.println("Spaces divide different numbers for example 1 + 2_1/2");
+		System.out.print("Enter an expression: ");
+		String input = in.nextLine();
+		// produceAnswer(input);
+		System.out.println(produceAnswer(input));
+		in.close();
+	}
 
-    	return check2;
-    	//return number;
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		// TODO: Implement this function to produce the solution to the input
+		int space1 = input.indexOf(" ");
+		String first = input.substring(0, space1);
+		char op = input.charAt(space1 + 1);
+		String last = input.substring(space1 + 3);
+		int sign1 = getSign(first);
+		int sign2 = getSign(last);
+		int whole1 = getWhole(first, sign1);
+		int whole2 = getWhole(last, sign2);
+		int numer1 = getNumer(first, sign1);
+		int numer2 = getNumer(last, sign2);
+		int denom1 = getDenom(first);
+		int denom2 = getDenom(last);
+		System.out.println(sign1);
+		System.out.println(sign2);
+		System.out.println(whole1);
+		System.out.println(whole2);
+		System.out.println(numer1);
+		System.out.println(numer2);
+		System.out.println(denom1);
+		System.out.println(denom2);
+		System.out.println();
+		numer1 = sign1 * getImproper(whole1, numer1, denom1);
+		System.out.println(numer1);
+		numer2 = sign2 * getImproper(whole2, numer2, denom2);
+		System.out.println(numer2);
+		String combined = "";
+		if (op == '+') {
+			combined = add(numer1, numer2, denom1, denom2);
+		} else if (op == '-') {
+			numer2 = -numer2;
+			combined = add(numer1, numer2, denom1, denom2);
+			System.out.println(combined);
+		} else if (op == '*') {
+			combined = multiply(numer1, numer2, denom1, denom2);
+		} else if (op == '/') {
+			if (numer2 < 0) {
+				numer2 = -numer2;
+				denom2 = -denom2;
+			}
+			combined = multiply(numer1, denom2, denom1, numer2);
+			System.out.println(combined);
+		}
+		int sign = getSign(combined);
+		int numer = getNumer(combined, sign);
+		int denom = getDenom(combined);
+		String result = simplify(sign, numer, denom);
+		return result;
+	}
+
+	public static int getSign(String whole) {
+		if (whole.charAt(0) != '-') {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	public static int getWhole(String in, int sign) {
+		int start = 0;
+		if (sign == -1) {
+			start = 1;
+		}
+		if (in.contains("_")) {
+			return Integer.parseInt(in.substring(start, in.indexOf("_")));
+		} else if (in.contains("/")) {
+			return 0;
+		} else {
+			return Integer.parseInt(in.substring(start, in.length()));
+		}
+	}
+
+	public static int getNumer(String in, int sign) {
+		int start = 0;
+		int end = in.indexOf("/");
+		if (sign == -1) {
+			start = 1;
+		}
+		if (in.contains("_")) {
+			start = in.indexOf("_") + 1;
+			return Integer.parseInt(in.substring(start, end));
+		} else if (in.contains("/")) {
+			return Integer.parseInt(in.substring(start, end));
+		} else {
+			return 0;
+		}
+	}
+
+	public static int getDenom(String in) {
+		if (in.contains("/")) {
+			return Integer.parseInt(in.substring(in.indexOf("/") + 1, in.length()));
+		} else {
+			return 1;
+		}
+	}
+
+	public static int getImproper(int whole, int numer, int denom) {
+		return whole * denom + numer;
+	}
+
+	public static String add(int n1, int n2, int d1, int d2) {
+		int n = (n1 * d2) + (n2 * d1);
+		int d = d1 * d2;
+		return n + "/" + d;
+	}
+
+	public static String multiply(int n1, int n2, int d1, int d2) {
+		int n = n1 * n2;
+		int d = d1 * d2;
+		return n + "/" + d;
+	}
+	public static String simplify(int sign, int n, int d) {
+		int whole = n / d;
+		n = n % d;
+		for (int i = 2; i < n; i++) {
+			while (n % i == 0 && d % i == 0) {
+				n /= i;
+				d /= i;
+			}
+		}
+		String sign_str = "";
+		if (sign == -1) {
+			sign_str = "-";
+		}
+		if (n > 0) {
+			return sign_str + whole + "_" + n + "/" + d;
+		} else {
+			return sign_str + whole;
+		}
+	}
 }
